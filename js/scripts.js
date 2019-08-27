65eeaab638bea6a27b24e924252786a4d31f86bd
danielevans-999 <danielevans.karani@gmail.comgit>
2019-08-27 16:11:27 +0300

    Adds user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,18 @@ $("form#new-contact").submit(function(event){
     var inputtedLastName=$("input#new-last-name").val();
     var newContact=new Contact(inputtedFirstName,inputtedLastName);
 
-    $("ul#conatcts").append("<li><span class='contact'>" + newContact.firstName + "</span><li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span><li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+
+    $(".contact").last().click(function(){
+        $("#show-contact").show();
+        $("show-contact h2").text(newContact.firstName);
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+    });
 });
+
+
 
