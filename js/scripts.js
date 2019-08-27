fe8dcadf7bd34335765cfdb995d3034bb83bef7c
danielevans-999 <danielevans.karani@gmail.comgit>
2019-08-27 15:28:27 +0300

    Adds user interface logic that collect and stores user contact inforamtion

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
+// bussiness logic
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
   }
+
+// user interface logic
+
+$("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var inputtedFirstName=$("input#new-first-name").val();
+    var inputtedLastName=$("input#new-last-name").val();
+    var newContact=new Contact(inputtedFirstName,inputtedLastName);
+
+    $("ul#conatcts").append("<li><span class='contact'>" + newContact.firstName + "</span><li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+});
+
